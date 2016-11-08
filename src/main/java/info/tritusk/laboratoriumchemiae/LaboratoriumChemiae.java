b4dc6f18d7c76fc36ae554b7721ee473b894cd42
3TUSK <urey.s.knowledge@gmail.com>
2016-11-07 21:51:48 -0500

    Ok. Lets make some unusual move.

---
+++ src/main/java/info/tritusk/laboratoriumchemiae/LaboratoriumChemiae.java
@@ -0,0 +1,8 @@
+package info.tritusk.laboratoriumchemiae;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "laboratoriumchemiae", name = "Laboratorium Chemiae")
+public class LaboratoriumChemiae {
+
+}
